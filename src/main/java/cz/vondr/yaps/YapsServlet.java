bb422dace562554ebb4180a7a8c92f35a3785eeb
bugs <bugs@localhost>
2014-12-12 21:35:00 +0100

    apache http components for request on target basic setup of httpClient

--- src/main/java/cz/vondr/yaps/YapsServlet.java
+++ src/main/java/cz/vondr/yaps/YapsServlet.java
@@ -1,5 +1,16 @@
 package cz.vondr.yaps;
 
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicHttpEntityEnclosingRequest;
+import org.apache.http.protocol.HttpContext;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,11 +19,77 @@ import java.io.IOException;
 
 public class YapsServlet extends HttpServlet {
 
+    protected CloseableHttpClient httpClient;
+
+    protected String target = "localhots:8080";
+
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        super.service(req, resp);
-        resp.setStatus(200);
-        resp.getOutputStream().print("Hello YapsServlet");
+    public void init() throws ServletException {
+        super.init();
+
+        createHttpClient();
+    }
+
+    private void createHttpClient() {
+
+        ConnectionKeepAliveStrategy keepAliveStrategy = new DefaultConnectionKeepAliveStrategy() {
+
+            @Override
+            public long getKeepAliveDuration(HttpResponse response, HttpContext context) {
+                long keepAlive = super.getKeepAliveDuration(response, context);
+                if (keepAlive == -1) {
+                    // Keep connections alive 5 seconds if a keep-alive value
+                    // has not be explicitly set by the server
+                    keepAlive = 5000; //TODO parameterize
+                }
+                return keepAlive;
+            }
+
+        };
+
+
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(200); //TODO parameterize
+        connectionManager.setDefaultMaxPerRoute(20); //TODO parameterize
+
+        //TODO find optimal client setup
+        httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .disableCookieManagement()
+                .setKeepAliveStrategy(keepAliveStrategy)
+                .build();
+
+
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        try {
+            httpClient.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse servletResponse) throws ServletException, IOException {
+        CloseableHttpResponse httpResponse = httpClient.execute(new HttpHost("twitter.com", 80), new BasicHttpEntityEnclosingRequest("GET", "http://twitter.com/jvondrous"));
+        try {
+            //TODO rewrite 'target response' to 'proxy response'
+            servletResponse.setStatus(200);
+//            servletResponse.getOutputStream().print("Hello YapsServlet");
+
+            httpResponse.getEntity().writeTo(servletResponse.getOutputStream());
+//            servletResponse.getOutputStream()
+//            httpResponse.getEntity().getContent()
+
+        } finally {
+            httpResponse.close();
+        }
+
+
     }
 
 }
