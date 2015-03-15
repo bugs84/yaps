1ea82b68cdade53d7534cc70288efbb126e6e637
bugs <bugs@localhost>
2015-03-15 20:06:48 +0100

    Test for headers with the same name

--- src/test/java/cz/vondr/yaps/unit_tests/Headers.groovy
+++ src/test/java/cz/vondr/yaps/unit_tests/Headers.groovy
@@ -1,5 +1,6 @@
 package cz.vondr.yaps.unit_tests
 
+import com.jcabi.http.request.ApacheRequest
 import com.jcabi.http.request.JdkRequest
 import cz.vondr.yaps.unit_tests.tool.JettyProxy
 import cz.vondr.yaps.unit_tests.tool.VertXTarget
@@ -8,7 +9,7 @@ import org.junit.Test
 class Headers implements VertXTarget, JettyProxy {
 
     @Test(timeout = 1000L)
-    void 'header are resend to target'() {
+    void 'header is resend to target'() {
         targetHandler = { req ->
             assert req.headers().get("TestHeader") == "Value of Test header"
             req.response().end()
@@ -18,7 +19,7 @@ class Headers implements VertXTarget, JettyProxy {
     }
 
     @Test(timeout = 1000L)
-    void 'multiple headers with diacritics are resend'() {
+    void 'multiple headers with diacritics are resend to target'() {
         targetHandler = { req ->
             assert req.headers().get("Žluťoučký kůň úpěl ďábelské ódy 1") == "value1"
             assert req.headers().get("key2") == "Žluťoučký kůň úpěl ďábelské ódy 2"
@@ -30,4 +31,19 @@ class Headers implements VertXTarget, JettyProxy {
                 .header("key2", "Žluťoučký kůň úpěl ďábelské ódy 2")
                 .fetch()
     }
+
+    @Test(timeout = 1000L)
+    void 'headers with same name is resend to target'() {
+        targetHandler = { req ->
+            assert req.headers().getAll("TestHeader") == ["Value 1", "Value 2"]
+            req.response().end()
+        }
+
+        new ApacheRequest("$proxyUrl")
+                .header("TestHeader", "Value 1")
+                .header("TestHeader", "Value 2")
+                .fetch()
+    }
+
+
 }
